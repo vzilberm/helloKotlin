588d3b965899b643426acc3087e359b5db5637ae
vzilberm <vzilbermints@gmail.com>
2020-09-11 11:04:31 -0500

    VZ & Nakib: trying to save this work

---
+++ src/main/kotlin/com/zmints/helloKotlin/controllers/GreetingController.kt
@@ -0,0 +1,18 @@
+package com.zmints.helloKotlin.controllers
+
+import com.zmints.helloKotlin.model.Greeting
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.atomic.AtomicLong
+
+@RestController
+class GreetingController {
+
+    val counter = AtomicLong()
+
+    @GetMapping("/greetings")
+    fun greetings(@RequestParam(value = "name", defaultValue = "World") name: String) =
+            Greeting(counter.incrementAndGet(), "Hello, $name")
+
+}
---
+++ src/main/kotlin/com/zmints/helloKotlin/model/Greetings.kt
@@ -0,0 +1,3 @@
+package com.zmints.helloKotlin.model
+
+data class Greeting(val id: Long, val content: String)
